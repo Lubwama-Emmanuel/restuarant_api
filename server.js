3c764ac40ff295bc3ab695bd55b1f2d9af1a95d6
Rex <lubwamaemmanuel1@gmail.com>
2023-06-07 17:27:51 +0300

    refactor:(alittle cleaning)

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const app = require('./app')
-const dotenv = require("dotenv")
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({path: "./config.env"})
+dotenv.config({ path: "./config.env" });
 
-const port = process.env.PORT
+const port = process.env.PORT;
+const DB = process.env.DATABASE_URL.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log(`DATABASE CONNECTED SUCCESSFULLY`);
+  })
+  .catch((err) => {
+    console.log(`Ooooops Database not connected`, err);
+  });
 
 app.listen(port, () => {
-    console.log(`----server running on port: ${port}`)
-})
+  console.log(`----server running on port: ${port}`);
+});

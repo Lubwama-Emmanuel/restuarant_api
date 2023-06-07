2539e71dbb826703f63f91a3cb02452880646e95
Rex <lubwamaemmanuel1@gmail.com>
2023-06-07 17:27:15 +0300

    refactor:(a little refactor)

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require("express")
-const logger = require("morgan")
+const express = require("express");
+const logger = require("morgan");
+const restuarantRouter = require("./routes/restuarantRouter");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(logger('dev'))
+app.use(express.json());
+app.use(logger("dev"));
 
+app.use("/api/v1", restuarantRouter);
 
-module.exports = app
+module.exports = app;

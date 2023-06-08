64b5f85351d3d5b345f6f9775ce597e3cf267900
Rex <lubwamaemmanuel1@gmail.com>
2023-06-08 12:49:33 +0300

    feat:(added upload image route)

--- routes/restuarantRouter.js
+++ routes/restuarantRouter.js
@@ -2,7 +2,11 @@ const express = require("express");
 const route = express.Router();
 const restuarantController = require("../controllers/restuarantController");
 
-route.post("/newRestuarant", restuarantController.createRestuarant);
+route.post(
+  "/newRestuarant",
+  restuarantController.uploadImage,
+  restuarantController.createRestuarant
+);
 route.patch("/updateRestuarant/:id", restuarantController.updateRestuarant);
 route.get("/getRestuarant/:id", restuarantController.getRestuarant);
 route.get("/getAllRestuarants", restuarantController.getAllRestuarants);

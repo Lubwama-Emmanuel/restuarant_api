e890e5f61e29046f7a9603ca212f7f0d612b357c
Rex <lubwamaemmanuel1@gmail.com>
2023-06-08 12:49:07 +0300

    feat:(added image upload)

--- controllers/restuarantController.js
+++ controllers/restuarantController.js
@@ -1,10 +1,50 @@
 const Restuarant = require("../models/restaurantModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
+const multer = require("multer");
+
+const multerStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/img");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const fileName = file.originalname + "-" + uniqueSuffix;
+    console.log(fileName);
+    cb(null, fileName);
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  console.log(file);
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image! Please upload only images.", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadImage = upload.single("image");
 
 // Create new restuarant
 exports.createRestuarant = catchAsync(async (req, res) => {
-  const newRestuarant = await Restuarant.create(req.body);
+  let body = {
+    name: req.body.name,
+    title: req.body.title,
+    cuisineType: req.body.cuisineType,
+    location: req.body.location,
+  };
+
+  if (req.file) {
+    body["image"] = req.file.path;
+  }
+
+  const newRestuarant = await Restuarant.create(body);
 
   res.status(201).json({
     status: "Success",

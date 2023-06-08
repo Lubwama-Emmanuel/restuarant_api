bf120abb627f4911fe51ecf72a42721d19e9195b
Rex <lubwamaemmanuel1@gmail.com>
2023-06-08 11:01:22 +0300

    feat:(added more restuarant routes)

--- routes/restuarantRouter.js
+++ routes/restuarantRouter.js
@@ -3,5 +3,9 @@ const route = express.Router();
 const restuarantController = require("../controllers/restuarantController");
 
 route.post("/newRestuarant", restuarantController.createRestuarant);
+route.patch("/updateRestuarant/:id", restuarantController.updateRestuarant);
+route.get("/getRestuarant/:id", restuarantController.getRestuarant);
+route.get("/getAllRestuarants", restuarantController.getAllRestuarants);
+route.delete("/deleteRestuarant/:id", restuarantController.deleteRestuarant);
 
 module.exports = route;

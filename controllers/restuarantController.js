f3a5656e4451864b8db1d7f0e91696b24df97b53
Rex <lubwamaemmanuel1@gmail.com>
2023-06-08 11:24:07 +0300

    test:(improved test coverage)

--- controllers/restuarantController.js
+++ controllers/restuarantController.js
@@ -1,24 +1,6 @@
 const Restuarant = require("../models/restaurantModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
-const multer = require("multer");
-
-const multerStorage = multer.memoryStorage();
-
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image")) {
-    cb(null, true);
-  } else {
-    cb(new AppError("Not an image! Please upload only images.", 400), false);
-  }
-};
-
-const upload = multer({
-  stuarge: multerStorage,
-  fileFilter: multerFilter,
-});
-
-exports.uploadImage = upload.single("image");
 
 // Create new restuarant
 exports.createRestuarant = catchAsync(async (req, res) => {

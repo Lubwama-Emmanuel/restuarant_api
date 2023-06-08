96615427c3bf2fc0a458359688377c940d90bd61
Rex <lubwamaemmanuel1@gmail.com>
2023-06-08 11:01:07 +0300

    feat:(added more controllers)

--- controllers/restuarantController.js
+++ controllers/restuarantController.js
@@ -20,13 +20,60 @@ const upload = multer({
 
 exports.uploadImage = upload.single("image");
 
+// Create new restuarant
 exports.createRestuarant = catchAsync(async (req, res) => {
   const newRestuarant = await Restuarant.create(req.body);
 
   res.status(201).json({
     status: "Success",
+    data: newRestuarant,
+  });
+});
+
+// Update restuarant details
+exports.updateRestuarant = catchAsync(async (req, res) => {
+  const restuarant = await Restuarant.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+
+  res.status(200).json({
+    status: "Success",
+    data: restuarant,
+  });
+});
+
+// Return restuarant data by id
+exports.getRestuarant = catchAsync(async (req, res) => {
+  const restuarant = await Restuarant.findById(req.params.id);
+
+  res.status(200).json({
+    status: "Success",
     data: {
-      data: newRestuarant,
+      data: restuarant,
     },
+  });
+});
+
+// Returns list of all restuarants
+exports.getAllRestuarants = catchAsync(async (req, res) => {
+  const restuarants = await Restuarant.find();
+
+  res.status(200).json({
+    status: "Success",
+    data: restuarants,
+  });
+});
+
+// Deletes restuarant by id
+exports.deleteRestuarant = catchAsync(async (req, res) => {
+  await Restuarant.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: "Success",
+    message: "restuarant deleted!",
   });
 });

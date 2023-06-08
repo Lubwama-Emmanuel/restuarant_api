45de0ad2e51e880cd55ca36ee6a3a9ca4452f82b
Rex <lubwamaemmanuel1@gmail.com>
2023-06-08 12:48:10 +0300

    refactor:(a little clean up)

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const logger = require("morgan");
 const restuarantRouter = require("./routes/restuarantRouter");
 const AppError = require("./utils/AppError");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(logger("dev"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1", restuarantRouter);
 
